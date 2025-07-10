2cba93a85b68093392fc28b27d44842c41b05e08
hohohozy <prinmeiho@gmail.com>
2025-07-10 19:20:03 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GameGrid from './GameGrid';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Escape Grid 逃出生格</h1>
+      <GameGrid />
+    </div>
+  );
+}
